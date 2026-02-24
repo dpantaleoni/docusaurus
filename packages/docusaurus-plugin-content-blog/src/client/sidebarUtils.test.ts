30aa976c489046443db5f56e07292e60037798c3
Armagan Sisik <armagansisik@gmail.com>
2026-02-24 13:26:49 -0800

    fix the comment block

--- packages/docusaurus-plugin-content-blog/src/client/sidebarUtils.test.ts
+++ packages/docusaurus-plugin-content-blog/src/client/sidebarUtils.test.ts
@@ -5,10 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-/**
- * @jest-environment jsdom
- */
-
 import * as Router from '@docusaurus/router';
 import {renderHook} from '@testing-library/react';
 import {
